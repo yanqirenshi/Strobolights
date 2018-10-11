6190f01c469717f382b882f3a54bd1cf79ef332b
satoshi.iwasaki <satoshi.iwasaki@glpgs.com>
2018-10-11 16:35:48 +0900

    update sample

--- src/app.sample.lisp
+++ src/app.sample.lisp
@@ -21,17 +21,5 @@
      nil)
  :session
  :validation
- (:mount "/ter/api/v1"    ter.api.router:*api-v1*)
- (:mount "/nobit@/api/v1" nobit@.api.api-v1:*router*)
- (:mount "/tatta/api/v1"  tatta-api.api-v1:*api-v1*)
- (:mount "/qupool/api/v1" qpool.api.api-v1:*router*)
- (:mount "/gp/api/v1"     gp.api.router:*api.v.1*)
- ;; AWS
- (:mount "/aws/api/v1/beach"  ahan-whun-shugoi-api.beach:*api-beach*)
- (:mount "/aws/api/v1/cosmos" ahan-whun-shugoi-api.cosmos:*api-cosmos*)
- (:mount "/aws/api/v1"        ahan-whun-shugoi-api.api-v1:*api-v1*)
- ;; gp
- (:mount "/gp/api/v1/" gp.api.router:*api.v.1*)
- ;; jira
- (:mount "/jira/api/v1/" api.jira.router:*v1*)
+ ;; (:mount "/your/api/path/" your.api.package:*route*)
  *route*)
